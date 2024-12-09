0dde92d1994feab8c506cf0fdca8698fc561f0ee
amitk7288 <amitkadara@gmail.com>
2024-12-09 13:55:00 +0000

    misc text update

--- src/components/mainview/skills/MySkills.jsx
+++ src/components/mainview/skills/MySkills.jsx
@@ -1,7 +1,7 @@
 import Skill from "./Skill"
 import { TbApi } from "react-icons/tb";
 import { RiLoopRightLine } from "react-icons/ri";
-import html from "src/assets/tool-icons/html-logo.png";
+import html from "/assets/tool-icons/html-logo.png";
 
 export default function MySkills() {
   return (
@@ -13,7 +13,7 @@ export default function MySkills() {
       </div>
       <div className="flex flex-wrap justify-center gap-5">
         <Skill logo={html} name={`HTML5`} />
-        <Skill logo="src/assets/tool-icons/css-logo.png" name={`CSS3`} />
+        <Skill logo="/assets/tool-icons/css-logo.png" name={`CSS3`} />
         <Skill logo="src/assets/tool-icons/js-logo.png" name={`JavaScript`} />
         <Skill logo="src/assets/tool-icons/react-logo.png" name={`React`} />
         <Skill logo="src/assets/tool-icons/redux-icon.webp" name={`Redux`} />
