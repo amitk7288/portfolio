afafaed4c54ea4827657d80ed36e2a8031721cac
amitk7288 <amitkadara@gmail.com>
2024-12-09 14:10:20 +0000

    multiple updates

--- src/components/mainview/skills/MySkills.jsx
+++ src/components/mainview/skills/MySkills.jsx
@@ -2,6 +2,20 @@ import Skill from "./Skill"
 import { TbApi } from "react-icons/tb";
 import { RiLoopRightLine } from "react-icons/ri";
 import html from "../../../assets/tool-icons/html-logo.png";
+import css from "../../../assets/tool-icons/css-logo.png";
+import js from "../../../assets/tool-icons/js-logo.png";
+import react from "../../../assets/tool-icons/react-logo.png";
+import redux from "../../../assets/tool-icons/redux-icon.webp";
+import mongo from "../../../assets/tool-icons/mongodb.svg";
+import express from "../../../assets/tool-icons/express.png";
+import node from "../../../assets/tool-icons/nodejs.png";
+import tailwind from "../../../assets/tool-icons/tailwind-logo.png";
+import sass from "../../../assets/tool-icons/sass-logo.png";
+import bootstrap from "../../../assets/tool-icons/Bootstrap.png";
+import github from "../../../assets/tool-icons/github-logo.png";
+import bitbucket from "../../../assets/tool-icons/bitbucket-logo.png";
+import trello from "../../../assets/tool-icons/trello.svg";
+import jira from "../../../assets/tool-icons/jira.png";
 
 export default function MySkills() {
   return (
@@ -13,24 +27,24 @@ export default function MySkills() {
       </div>
       <div className="flex flex-wrap justify-center gap-5">
         <Skill logo={html} name={`HTML5`} />
-        <Skill logo="../../../assets/tool-icons/css-logo.png" name={`CSS3`} />
-        <Skill logo="src/assets/tool-icons/js-logo.png" name={`JavaScript`} />
-        <Skill logo="src/assets/tool-icons/react-logo.png" name={`React`} />
-        <Skill logo="src/assets/tool-icons/redux-icon.webp" name={`Redux`} />
-        <Skill logo="src/assets/tool-icons/mongodb.svg" name={`MongoDB`} />
-        <Skill logo="src/assets/tool-icons/express.png" name={`Express`} />
-        <Skill logo="src/assets/tool-icons/nodejs.png" name={`Node JS`} />
-        <Skill logo="src/assets/tool-icons/tailwind-logo.png" name={`Tailwind`} />
-        <Skill logo="src/assets/tool-icons/sass-logo.png" name={`Sass`} />
-        <Skill logo="src/assets/tool-icons/Bootstrap.png" name={`Bootstrap`} />
-        <Skill logo="src/assets/tool-icons/github-logo.png" name={`Git/Github`} />
-        <Skill logo="src/assets/tool-icons/bitbucket-logo.png" name={`Bitbucket`} />
+        <Skill logo={css} name={`CSS3`} />
+        <Skill logo={js} name={`JavaScript`} />
+        <Skill logo={react} name={`React`} />
+        <Skill logo={redux} name={`Redux`} />
+        <Skill logo={mongo} name={`MongoDB`} />
+        <Skill logo={express} name={`Express`} />
+        <Skill logo={node} name={`Node JS`} />
+        <Skill logo={tailwind} name={`Tailwind`} />
+        <Skill logo={sass} name={`Sass`} />
+        <Skill logo={bootstrap} name={`Bootstrap`} />
+        <Skill logo={github} name={`Git/Github`} />
+        <Skill logo={bitbucket} name={`Bitbucket`} />
         <div className="border w-fit flex items-center gap-2 py-2 px-3 rounded-lg ">
           <div><RiLoopRightLine className="w-[20px] text-white bg-[#2983fd]" /></div>
           <div><p className="text-lg sm:text-xl font-medium text-white">Agile</p></div>
         </div>
-        <Skill logo="src/assets/tool-icons/trello.svg" name={`Trello`} />
-        <Skill logo="src/assets/tool-icons/jira.png" name={`Jira`} />
+        <Skill logo={trello} name={`Trello`} />
+        <Skill logo={jira} name={`Jira`} />
         <div className="border w-fit flex items-center gap-2 py-2 px-3 rounded-lg ">
           <div><TbApi className="w-[20px] text-black bg-lime-400" /></div>
           <div><p className="text-lg sm:text-xl font-medium text-white">API&apos;s</p></div>
