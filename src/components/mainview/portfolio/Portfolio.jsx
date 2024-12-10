92f778786dab1c2c7b93e431a90e64a8e2c2bbd6
amitk7288 <amitkadara@gmail.com>
2024-12-10 14:10:47 +0000

    multiple updates

--- src/components/mainview/portfolio/Portfolio.jsx
+++ src/components/mainview/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "./projectCard/ProjectCard";
 
 export default function Portfolio() {
   return (
-    <div className="px-8 my-6 lg:my-20 xs:px-10 lg:px-14 xl:px-20 2xl:px-40 3xl:px-60">
+    <div id="projects" className="px-8 my-6 lg:my-20 xs:px-10 lg:px-14 xl:px-20 2xl:px-40 3xl:px-60">
       <div className="flex flex-col gap-1 mb-4">
         <h2 className="text-[#111827] text-2xl font-bold lg:text-[36px]">Portfolio</h2>
         <p className="font-normal text-[#374151] lg:text-[20px] lg:leading-[36px]">A selection of some of my projects</p>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,10 @@
 import react from "../assets/tool-icons/react-logo.png";
+import angular from "../assets/tool-icons/angular_gradient.png";
 import redux from "../assets/tool-icons/redux-icon.webp";
 import tailwind from "../assets/tool-icons/tailwind-logo.png";
 import reactIcons from "../assets/tool-icons/react-icons-icon.svg";
 import github from "../assets/tool-icons/github-logo.png";
+import bitbucket from "../assets/tool-icons/bitbucket-logo.png";
 import js from "../assets/tool-icons/js-logo.png";
 import sass from "../assets/tool-icons/sass-logo.png";
 import boot from "../assets/tool-icons/Bootstrap.png";
@@ -13,11 +15,14 @@ import mongodb from "../assets/tool-icons/mongodb.svg";
 import express from "../assets/tool-icons/express.png";
 import firebase from "../assets/tool-icons/firebase.png";
 import cloudinary from "../assets/tool-icons/cloudinary.webp";
+import jira from "../assets/tool-icons/jira.png";
+import postman from "../assets/tool-icons/postman.svg";
 
 import mern from "../assets/mern-reed-clone.png";
 import gamedb from "../assets/react-game-db-img-drk.png";
 import kanban from "../assets/react-kanban-light.png";
 import timeline from "../assets/nx-50.png";
+import nxjp from "../assets/NX-Journey-Planner.png";
 
 
 const projects = [
@@ -29,35 +34,51 @@ const projects = [
   desc: `A full-stack MERN web application featuring Google OAuth and JWT for secure authentication, Cloudinary and Multer for file uploads, and bcrypt for password encryption. The app featurs CRUD operations, real-time updates, and a responsive design, delivering a seamless and modern user experience.`,
   tools: [
     {id: 1, icon: mongodb, alt: 'Mongo DB'},
-    {id: 1, icon: express, alt: 'Express'},
-    {id: 1, icon: react, alt: 'React'},
-    {id: 3, icon: node, alt: 'Node JS'},
-    {id: 2, icon: redux, alt: 'Redux'},
-    {id: 4, icon: tailwind, alt: 'Tailwind'},
-    {id: 5, icon: reactIcons, alt: 'React Icons'},
-    {id: 5, icon: api, alt: 'Reed API'},
-    {id: 5, icon: firebase, alt: 'Google Firebase'},
-    {id: 5, icon: cloudinary, alt: 'Cloudinary'},
-    {id: 6, icon: github, alt: 'Github'},
+    {id: 2, icon: express, alt: 'Express'},
+    {id: 3, icon: react, alt: 'React'},
+    {id: 4, icon: node, alt: 'Node JS'},
+    {id: 5, icon: redux, alt: 'Redux'},
+    {id: 6, icon: tailwind, alt: 'Tailwind'},
+    {id: 7, icon: reactIcons, alt: 'React Icons'},
+    {id: 8, icon: api, alt: 'Reed API'},
+    {id: 9, icon: firebase, alt: 'Google Firebase'},
+    {id: 10, icon: cloudinary, alt: 'Cloudinary'},
+    {id: 11, icon: github, alt: 'Github'},
+    {id: 12, icon: postman, alt: 'Postman'},
   ],
   },
-    {
+ {
     id: 2,
-    title: "React Game DB (API)",
-    img: gamedb,
-    link: "https://amits-react-gamedb.netlify.app/",
-    desc: `A fun project I built with a modern web stack - React JS, Redux for global state management, React Router for navigation, Tailwind CSS for styling and the RAWG API. Check it out below!`,
+    title: "National Express Journey Planner (1M + Users)",
+    img: nxjp,
+    link: "https://book.nationalexpress.com/coach/#/choose-journey",
+    desc: `I was tasked to work on the National Express Journey Planner, which is used by millions of users. The journey planner itself required me to work with Angular and Sass, and we worked together on this project using Git and BitBucket.`,
     tools: [
-      {id: 1, icon: react, alt: 'React'},
-      {id: 2, icon: redux, alt: 'Redux'},
-      {id: 3, icon: tailwind, alt: 'Tailwind'},
-      {id: 4, icon: reactIcons, alt: 'React Icons'},
-      {id: 5, icon: api, alt: 'RAWG API'},
-      {id: 6, icon: github, alt: 'Github'},
+      {id: 1, icon: angular, alt: 'angular'},
+      {id: 2, icon: sass, alt: 'sass'},
+      {id: 3, icon: js, alt: 'javascript'},
+      {id: 4, icon: bitbucket, alt: 'bitbucket'},
+      {id: 5, icon: jira, alt: 'jira'},
     ],
   },
   {
-    id: 3,
+  id: 3,
+  title: "React Game DB (API)",
+  img: gamedb,
+  link: "https://amits-react-gamedb.netlify.app/",
+  desc: `A fun project I built with a modern web stack - React JS, Redux for global state management, React Router for navigation, Tailwind CSS for styling and the RAWG API. Check it out below!`,
+  tools: [
+    {id: 1, icon: react, alt: 'React'},
+    {id: 2, icon: redux, alt: 'Redux'},
+    {id: 3, icon: tailwind, alt: 'Tailwind'},
+    {id: 4, icon: reactIcons, alt: 'React Icons'},
+    {id: 5, icon: api, alt: 'RAWG API'},
+    {id: 6, icon: github, alt: 'Github'},
+    {id: 7, icon: postman, alt: 'Postman'},
+  ],
+  },
+  {
+    id: 4,
     title: "React Kanban Board",
     img: kanban,
     link: "https://amits-react-kanbanboard.netlify.app/",
@@ -69,9 +90,9 @@ const projects = [
       {id: 4, icon: reactIcons, alt: 'reactIcons'},
       {id: 5, icon: github, alt: 'github'},
     ],
-  },
+  }, 
   {
-    id: 4,
+    id: 5,
     title: "National Express 50th Timeline",
     img: timeline,
     link: "https://www.nationalexpress.com/en/timeline",
@@ -79,10 +100,11 @@ const projects = [
     tools: [
       {id: 1, icon: html, alt: 'html'},
       {id: 2, icon: sass, alt: 'sass'},
-      {id: 2, icon: js, alt: 'javascript'},
+      {id: 3, icon: js, alt: 'javascript'},
       {id: 4, icon: boot, alt: 'bootstrap'},
     ],
   },
+
 ];
 
 export default projects;
