2fe888070f4677773c31f8db370de11d81239ec2
amitk7288 <amitkadara@gmail.com>
2024-12-10 12:05:13 +0000

    added footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,17 +10,17 @@ export default function Footer() {
       <span className="h-0.5 w-[100px] bg-white"></span>
       <p className="md:text-lg">Please feel free to get in touch below:</p>
       <div className="flex flex-col items-center gap-3 sm:flex-row">
-        <button className="rounded-md border-2 border-white bg-white text-black px-7 py-3 text-sm tracking-widest">07841 579 569</button>
-        <button className="rounded-md border-2 border-white bg-black text-white px-7 py-3 text-sm tracking-widest">amitkadara@gmail.com</button>
+        <a href="tel:07841578679" className="rounded-md border-2 border-white bg-white text-black px-7 py-3 text-sm tracking-widest">07841 579 569</a>
+        <a href="mailto:amitkadara@gmail.com" className="rounded-md border-2 border-white bg-black text-white px-7 py-3 text-sm tracking-widest">amitkadara@gmail.com</a>
       </div>
     </div>
     <div id="info" className="flex flex-col items-center gap-5 py-12 px-6 bg-custom-pattern bg-custom-pos bg-custom-size h-fit w-full">
       <div className="flex items-center gap-4">
-        <FaLinkedin className="text-2xl h-7 w-7 object-contain" />
-        <FaGithubSquare className="text-2xl h-7 w-7 object-contain" />
-        <BiSolidEnvelope className="text-2xl h-7 w-7 object-contain" />
+         <a href="https://www.linkedin.com/in/amitkadara/" target="_blank" className="cursor-pointer"><FaLinkedin className="text-2xl h-7 w-7 object-contain" /></a>
+         <a href="https://github.com/amitk7288" target="_blank" className="cursor-pointer"><FaGithubSquare className="text-2xl h-7 w-7 object-contain" /></a>
+         <a href="mailto:amitkadara@gmail.com" target="_blank" className="cursor-pointer"><BiSolidEnvelope className="text-2xl h-7 w-7 object-contain" /></a>
       </div>
-      <p className="text-xs">Amit Kadara 2024 | Dev Dreamer</p>
+      <p className="text-xs">Amit Kadara 2024 | <a href="https://www.youtube.com/@DevDreamer" target="_blank">Dev Dreamer</a> </p>
     </div>
    </div>
   )
